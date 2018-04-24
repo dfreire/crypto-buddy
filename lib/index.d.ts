09cda1f8f9ad78685e7828a8bd2a4440bbcacb20
dfreire <dario.freire@gmail.com>
2018-04-24 17:52:20 +0100

    added typescript typings

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export declare function encrypt(key: any, text: any): string;
+export declare function decrypt(key: any, text: any): string;
+export declare function hashPass(password: any): any;
+export declare function sha1(text: any): string;
