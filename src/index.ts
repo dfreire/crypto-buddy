0b343b2d8f0b7057a7127b644941b4bf45e9f076
dfreire <dario.freire@gmail.com>
2018-04-24 16:32:07 +0100

    created repo

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as crypto from 'crypto';
+
+// credit: http://vancelucas.com/blog/stronger-encryption-and-decryption-in-node-js/
+export function encrypt(key, text) {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv('aes-256-cbc', new Buffer(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+// credit: http://vancelucas.com/blog/stronger-encryption-and-decryption-in-node-js/
+export function decrypt(key, text) {
+    const textParts = text.split(':');
+    const iv = new Buffer(textParts.shift(), 'hex');
+    const encryptedText = new Buffer(textParts.join(':'), 'hex');
+    const decipher = crypto.createDecipheriv('aes-256-cbc', new Buffer(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
